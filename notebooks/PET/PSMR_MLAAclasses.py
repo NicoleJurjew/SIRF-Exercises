e75291256866806cee5def30f7a77f9db8a974a7
NicoleJurjew <nicole.jurjew.20@ucl.ac.uk>
2024-04-09 15:45:10 +0100

    BSREM.py: as is on SIRF_Contribs by Kris PSMR_MLAAclasses.py: raw "skeleton", no specific methods implemented

--- notebooks/PET/PSMR_MLAAclasses.py
+++ notebooks/PET/PSMR_MLAAclasses.py
@@ -13,114 +13,75 @@ from sirf.Utilities import examples_data_path
 
 from cil.optimisation.algorithms import Algorithm 
 
-class BSREMSkeleton(Algorithm):
-    def __init__(self, data, initial, initial_step_size, relaxation_eta, **kwargs):
-        super(BSREMSkeleton, self).__init__(**kwargs)
-        self.x = initial.copy()
-        self.data = data
-        self.num_subsets = len(data)
-        self.initial_step_size = initial_step_size
-        self.relaxation_eta = relaxation_eta
-        # compute small number to add to image in preconditioner
-        # don't make it too small as otherwise the algorithm cannot recover from zeroes.
-        self.eps = initial.max()/1e3
-        self.average_sensitivity = initial.get_uniform_copy(0)
-        for s in range(len(data)):
-            self.average_sensitivity += self.subset_sensitivity(s)/self.num_subsets
-        # add a small number to avoid division by zero in the preconditioner
-        self.average_sensitivity += self.average_sensitivity.max()/1e4
+class __MLAA_step__(Algorithm):
+    def __init__(self, acq_data, acq_models_act, initial, const_term, det_effs, prior_strength, **kwargs):
+        super(__MLAA_step__, self).__init__(**kwargs) #update_objective_interval=1, max_iteration=None, log_file=None
+        self.x = initial.clone() # self.x is always the current estimate, as I understand.
+        self.acq_data = acq_data
+        self.const_term = const_term
+        self.det_effs = det_effs
+        self.att_factors = det_effs.get_uniform_copy(1)
+        self.num_subsets = len(acq_data)
         self.subset = 0
-        self.FOV_filter = STIR.TruncateToCylinderProcessor()
-        self.configured = True
-
-    def subset_sensitivity(self, subset_num):
-        raise NotImplementedError
-
-    def subset_gradient(self, x, subset_num):
-        raise NotImplementedError
-
-    def epoch(self):
-        return self.iteration // self.num_subsets
-
-    def step_size(self):
-        return self.initial_step_size / (1 + self.relaxation_eta * self.epoch())
+        self.configured = True # I don't know what implications this has specifically, but fine by me
+        self.acq_model_activity = acq_models_act[self.subset]
+        self.prior_strength = prior_strength
 
     def update(self):
-        g = self.subset_gradient(self.x, self.subset)
-        self.x_update = (self.x + self.eps) * g / self.average_sensitivity * self.step_size()
-        self.FOV_filter.apply(self.x_update)
-        self.x += self.x_update
-        # threshold to non-negative
-        self.x.maximum(0, out=self.x)
-        self.subset = (self.subset + 1) % self.num_subsets
+        raise NotImplementedError
 
     def update_objective(self):
-        # required for current CIL (needs to set self.loss)
-        self.loss.append(self.objective_function(self.x))
+        raise NotImplementedError
 
     def objective_function(self, x):
         ''' value of objective function summed over all subsets '''
         v = 0
-        for s in range(len(self.data)):
+        for s in range(len(self.acq_data)):
             v += self.subset_objective(x, s)
         return v
 
     def subset_objective(self, x, subset_num):
         ''' value of objective function for one subset '''
         raise NotImplementedError
+    
+    def get_QP_and_grad(self, x):
+        ''' returns QuadraticPrior and its gradient '''
+        qp = STIR.QuadraticPrior()
+        qp.set_penalisation_factor(self.prior_strength)
+        qp.set_up(x)
+        qp_grad = qp.get_gradient(x)
 
-class BSREM1(BSREMSkeleton):
-    ''' BSREM implementation using sirf.STIR objective functions'''
-    def __init__(self, data, obj_funs, initial, initial_step_size=1, relaxation_eta=0, **kwargs):
+        return qp, qp_grad
+
+
+    
+
+class MLEM(__MLAA_step__):
+    ''' MLEM step'''
+    def __init__(self, acq_data, acq_models_act, initial, const_term, det_effs, prior_strength, **kwargs):
         '''
-        construct Algorithm with lists of data and, objective functions, initial estimate, initial step size,
-        step-size relaxation (per epoch) and optionally Algorithm parameters
         '''
-        self.obj_funs = obj_funs
-        super(BSREM1, self).__init__(data, initial, initial_step_size, relaxation_eta, **kwargs)
+        super(MLEM, self).__init__(**kwargs)
 
-    def subset_sensitivity(self, subset_num):
-        ''' Compute sensitivity for a particular subset'''
-        self.obj_funs[subset_num].set_up(self.x)
-        # note: sirf.STIR Poisson likelihood uses `get_subset_sensitivity(0) for the whole
-        # sensitivity if there are no subsets in that likelihood
-        return self.obj_funs[subset_num].get_subset_sensitivity(0)
 
-    def subset_gradient(self, x, subset_num):
-        ''' Compute gradient at x for a particular subset'''
-        return self.obj_funs[self.subset].gradient(x)
+    
 
-    def subset_objective(self, x, subset_num):
-        ''' value of objective function for one subset '''
-        return self.obj_funs[subset_num](x)
-
-class BSREM2(BSREMSkeleton):
-    ''' BSREM implementation using acquisition models and prior'''
-    def __init__(self, data, acq_models, prior, initial, initial_step_size=1, relaxation_eta=0, **kwargs):
+class MLTR(__MLAA_step__):
+    ''' MLTR step'''
+    def __init__(self, nonTOF_template, acq_models_att, step_size, **kwargs):
         '''
-        construct Algorithm with lists of data and acquisition models, prior, initial estimate, initial step size,
-        step-size relaxation (per epoch) and optionally Algorithm parameters.
-
-        WARNING: This version will use the same prior in each subset without rescaling. You should
-        therefore rescale the penalisation_factor of the prior before calling this function. This will
-        change in the future.
         '''
-        self.acq_models = acq_models
-        self.prior = prior
-        super(BSREM2, self).__init__(data, initial, initial_step_size, relaxation_eta, **kwargs)
+        super(MLTR, self).__init__(**kwargs)
 
-    def subset_sensitivity(self, subset_num):
-        ''' Compute sensitivity for a particular subset'''
-        self.acq_models[subset_num].set_up(self.data[subset_num], self.x)
-        return self.acq_models[subset_num].backward(self.data[subset_num].get_uniform_copy(1))
+    def prior(self, x):
+        ''' value of prior function '''
+        raise NotImplementedError
+    
+    def prior_gradient(self, x):
+        ''' gradient of prior function '''
+        raise NotImplementedError
+    
 
-    def subset_gradient(self, x, subset_num):
-        ''' Compute gradient at x for a particular subset'''
-        f = self.acq_models[subset_num].forward(x)
-        quotient = self.data[subset_num] / f
-        return self.acq_models[subset_num].backward(quotient - 1) - self.prior.gradient(x)
-
-    def subset_objective(self, x, subset_num):
-        ''' value of objective function for one subset '''
-        f = self.acq_models[subset_num].forward(x)
-        return self.data[subset_num].dot(f.log()) - f.sum() - self.prior(x)
+class MLAA(Algorithm):
+    def __init__(self, **kwargs):
+        super(MLAA, self).__init__(**kwargs)
